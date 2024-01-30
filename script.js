f8231ed93e1301268f2fe29b76e3caef30226388
seshadhri007 <158144140+seshadhri007@users.noreply.github.com>
2024-01-30 14:27:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// You can add JavaScript functionality here, for example:
+document.addEventListener('DOMContentLoaded', function() {
+    // Code to interact with the webpage
+    console.log('Webpage loaded!');
+});
